a94466bc613515d39f51cc055d2383505fcfba67
Tushar Patidar <patidartushar78@gmail.com>
2024-06-01 15:55:38 +0530

    corrected username already exist

--- controller/users.js
+++ controller/users.js
@@ -39,9 +39,6 @@ module.exports.renderSignupForm = (req, res) => {
 module.exports.signup = async (req, res) => {
     try {
         let { username, email, password } = req.body;
-        const newUser = new User({ email, username });
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
 
         // Generate OTP and send to email
         const otp = generateRandomNumber();
